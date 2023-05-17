38de3b04bb712418c9317fd51fb54721751ecb67
Ionut <81566145+Blitz-Cloud@users.noreply.github.com>
2023-05-17 19:54:33 +0300

    Fix Hero section styling fix

--- site/client/src/components/Form.jsx
+++ site/client/src/components/Form.jsx
@@ -1,15 +1,24 @@
 const Form = () => {
   return (
-    <div className=" rounded-md bg-white  bg-opacity-10 px-6 py-12 text-gray-900 backdrop-blur-md ">
+    <div className="m-4 rounded-md bg-white bg-opacity-10 px-4 py-8  text-gray-900 backdrop-blur-md">
       <h2 className="mb-1 text-xl font-semibold">Note muzicale</h2>
       <p className="mb-1 text-sm text-gray-900">
         Completeaza formularul de mai jos pentru a iti auzi melodiile preferate
         in timpul pauzelor
       </p>
       <form className="flex flex-col  " action="" method="post">
-        <input className="mb-2 mr-2 block rounded-sm bg-white/50 p-1 outline outline-1" />
-        <input className="mb-2 mr-2 block rounded-sm bg-white/50 p-1 outline outline-1" />
-        <input className="mb-2 mr-2 block rounded-sm bg-white/50 p-1 outline outline-1" />
+        <input
+          placeholder="YouTube Link"
+          className="mb-2 mr-2 block rounded-sm bg-white/50 p-1 outline outline-1"
+        />
+        <input
+          placeholder="YouTube Link"
+          className="mb-2 mr-2 block rounded-sm bg-white/50 p-1 outline outline-1"
+        />
+        <input
+          placeholder="YouTube Link"
+          className="mb-2 mr-2 block rounded-sm bg-white/50 p-1 outline outline-1"
+        />
         <a
           className="mt-auto self-start  rounded-sm bg-purple-800 px-4 py-2 font-medium tracking-wide text-white"
           href=""
--- site/client/src/components/Hero.jsx
+++ site/client/src/components/Hero.jsx
@@ -1,54 +1,35 @@
 import Form from "./Form";
 
-export const Hero = () => {
+const Hero = () => {
   return (
-    <header className="relative ">
-      <img
-        className="absolute inset-0 h-full w-full object-cover "
-        src="https://cnaic.ro/wp-content/uploads/2019/12/slider-bg.jpg"
-        alt=""
-        srcset=""
-      />
-      <div className="relative flex h-[100vh]">
-        <svg
-          className="absolute inset-x-0 bottom-0 fill-[#f5f5f5] text-white"
-          viewBox="0 0 1160 160 "
-        >
-          <path d="M-164 13L-104 39.7C-44 66 76 120 196 141C316 162 436 152 556 119.7C676 88 796 34 916 13C1036 -8 1156 2 1216 7.7L1276 13V162.5H1216C1156 162.5 1036 162.5 916 162.5C796 162.5 676 162.5 556 162.5C436 162.5 316 162.5 196 162.5C76 162.5 -44 162.5 -104 162.5H-164V13Z" />
-        </svg>
-        <div className="row grid grid-cols-2 items-center   gap-16  overflow-hidden px-8 py-16 text-white">
-          <div className="rounded-md bg-white bg-opacity-10 px-6 py-12 text-gray-900 backdrop-blur-md">
-            <h1 className="mb-3  text-3xl font-bold  ">
-              Pauze <span className="text-purple-800">muzicale</span>
-            </h1>
-            <p className="mb-5 mr-8  text-base text-gray-900 md:text-lg">
-              Școala noastră este un loc vibrant, plin de energie și
-              creativitate, iar în timpul pauzelor, holurile liceului se
-              transformă într-un adevărat spectacol muzical. Sunetele
-              încântătoare ale instrumentelor și vocile talentate ale elevilor
-              se întrepătrund într-un cadru plin de bucurie și entuziasm.
-            </p>
-            <a
-              className="rounded-sm bg-purple-800 px-4 py-2 font-medium tracking-wide text-white"
-              href=""
-            >
-              Google Btn
-            </a>
-            <a className="px-4 py-2 font-medium tracking-wide " href="">
-              Afla mai multe
-            </a>
-          </div>
-          <Form />
+    <div className="headerImage flex h-[100vh] items-center justify-center">
+      <header className="grid  grid-cols-2   gap-16 bg-cover bg-center bg-no-repeat">
+        <div className="m-4  rounded-md bg-white bg-opacity-10 px-4  py-8 text-gray-900 backdrop-blur-md">
+          <h1 className="mb-3  text-3xl font-bold  ">
+            Pauze <span className="text-purple-800">muzicale</span>
+          </h1>
+          <p className="mb-5 mr-8  text-base text-gray-900 md:text-lg">
+            Școala noastră este un loc vibrant, plin de energie și creativitate,
+            iar în timpul pauzelor, holurile liceului se transformă într-un
+            adevărat spectacol muzical. Sunetele încântătoare ale instrumentelor
+            și vocile talentate ale elevilor se întrepătrund într-un cadru plin
+            de bucurie și entuziasm.
+          </p>
+
+          <a
+            className="rounded-sm bg-purple-800 px-4 py-2 font-medium tracking-wide text-white"
+            href=""
+          >
+            Google Btn
+          </a>
+          <a className="px-4 py-2 font-medium tracking-wide " href="">
+            Afla mai multe
+          </a>
         </div>
-      </div>
-    </header>
+        <Form />
+      </header>
+    </div>
   );
 };
 
-// cnaic img
-/* <img
-          className=" object-cover w-full  h-[100%] "
-          src="https://cnaic.ro/wp-content/uploads/2019/12/slider-bg.jpg"
-          alt=""
-          srcset=""
-        /> */
+export default Hero;
