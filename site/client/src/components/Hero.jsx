6ccc3dcc4c8240555870bc788c436ee1d9fb6881
Ionut <81566145+Blitz-Cloud@users.noreply.github.com>
2023-05-20 19:33:05 +0300

    Update Added Google Btn for authentication
    Pe langa faptul ca am adaugat acel buton am facut mici modificari la
    configuratia pt Vite acuma in mod predefinit asculta pe localhost:8080
    Trebuie sa implementez o metoda de a autentifica utilizatorii pe
    platforma

--- site/client/src/components/Hero.jsx
+++ site/client/src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import Form from "./Form";
+import GoogleBtn from "./GoogleBtn";
 
 const Hero = () => {
   return (
     <div className="headerImage flex min-h-[100vh] items-center justify-center ">
       <header className="grid gap-8  bg-cover   bg-center bg-no-repeat p-4 lg:grid-cols-2">
-        <div className="m-4  rounded-md bg-white bg-opacity-10 px-4  py-8 text-gray-900 backdrop-blur-md">
+        <div className="m-4  rounded-md bg-white bg-opacity-10 px-4  py-6 text-gray-900 backdrop-blur-md">
           <h1 className="mb-3  text-3xl font-bold  ">
             Pauze <span className="text-purple-800">muzicale</span>
           </h1>
@@ -15,18 +16,14 @@ const Hero = () => {
             și vocile talentate ale elevilor se întrepătrund într-un cadru plin
             de bucurie și entuziasm.
           </p>
-
-          <a
-            className="rounded-sm bg-purple-800 px-4 py-2 font-medium tracking-wide text-white"
-            href=""
-          >
-            Google Btn
-          </a>
-          <a className="px-4 py-2 font-medium tracking-wide " href="">
-            Afla mai multe
-          </a>
+          <div className="flex">
+            <GoogleBtn />
+            <a className="px-4 py-2 font-medium tracking-wide " href="">
+              Afla mai multe
+            </a>
+          </div>
         </div>
-        <Form />
+        {/* <Form /> */}
       </header>
     </div>
   );
