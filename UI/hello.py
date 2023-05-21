720b4f4a019ce047e1df58284ca6d3542a0f4cae
Mishu <89389790+Mishu12111@users.noreply.github.com>
2023-05-21 18:18:09 +0300

    Update_3Actiuni
    Practic am adaugat buton de filtrare a melodiilor,de start a algoritmului si de stopare a programelor

--- UI/hello.py
+++ UI/hello.py
@@ -1,9 +1,2 @@
-import customtkinter
 
 print("Hello World")
-entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password",show="*")
-entry2.pack(pady=12, padx=10)
-
-
-for variabile in data['variabile']:
-    var=variabile['nr_pauze']
